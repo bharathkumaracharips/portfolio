64b19f92a7ed2b9173db38628965b51fbcb3bee6
P S BHARATH KUMAR ACHARI <bharathkumaracharips@gmail.com>
2025-02-11 17:08:13 +0530

    build edit02

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,37 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
 
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
+
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+// ];
+
+// export default eslintConfig;
+
+
+// .eslintrc.mjs
+
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+
+// Get the current file's directory name using ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+// Initialize FlatCompat with the base directory
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+// Extend default ESLint configurations
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,12 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  
   images: {
     domains: ['assets.aceternity.com', 'images.unsplash.com', 'api.microlink.io'], // Add Unsplash domain here
   },
-  "extends": ["next", "eslint:recommended", "plugin:react/recommended", "plugin:react-hooks/recommended"],
-  "rules": {
-    "react-hooks/exhaustive-deps": "off" 
-   }, // Disables the rule
-  // other config options can go here
+  output: 'export',
+  
 }
 
 export default nextConfig
